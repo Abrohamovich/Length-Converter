1e4db2bd8d317ab73c44ba120ca0c2073804d85c
Mykyta Herasymov <gerasimov08nikita@gmail.com>
2024-09-24 21:59:04 +0300

    Add kilometers to mile convertation

--- src/app/Main.java
+++ src/app/Main.java
@@ -7,17 +7,24 @@ public class Main {
     public static void main(String[] args) {
 
         System.out.println("Length converter application");
-        System.out.println("Version 0.1");
+        System.out.println("Version 1");
 
         double miles = 13;
         double kilometers = MileToKilometer(miles);
+        double km = 17;
+        double mls = KilometerToMile(km);
 
-        System.out.printf(miles + " miles is " + kilometers + " kilometers");
+        System.out.printf(miles + " miles is " + kilometers + " kilometers, and " +
+                km + " kilometers is " + mls + " miles");
 
     }
 
     private static double MileToKilometer(double miles) {
         return miles / CONV;
+    }
+
+    private static double KilometerToMile(double km) {
+        return km * CONV;
     }
 
 }
